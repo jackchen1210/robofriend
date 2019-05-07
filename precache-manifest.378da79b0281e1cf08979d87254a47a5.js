e5adcd8a01644f3ab96cd8a2a1275f02db3a8640
Jackchen <jack3492843@gmail.com>
2019-05-07 11:23:41 +0800

    Updates

---
+++ precache-manifest.378da79b0281e1cf08979d87254a47a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9deaf2bb1399eedf4f636df83f1e35c",
+    "url": "/robofriend/index.html"
+  },
+  {
+    "revision": "abd591b46b105fa8cbaf",
+    "url": "/robofriend/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "b476557046a1e6e97252",
+    "url": "/robofriend/static/css/main.7aa1e59e.chunk.css"
+  },
+  {
+    "revision": "abd591b46b105fa8cbaf",
+    "url": "/robofriend/static/js/2.24844647.chunk.js"
+  },
+  {
+    "revision": "b476557046a1e6e97252",
+    "url": "/robofriend/static/js/main.654dc326.chunk.js"
+  },
+  {
+    "revision": "75cc8018399ebb82c64f",
+    "url": "/robofriend/static/js/runtime~main.f5f06c82.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriend/static/media/SEGA.12a3db8b.woff"
+  }
+]);
